047802425be56d818440e3129f6ab8f729b4e28a
Arpit Jana <arpitjana2103@gmail.com>
2025-12-26 13:14:07 +0530

    converting useState to useReducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import reactDom from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 const rootEl = document.getElementById("root");
 const root = reactDom.createRoot(rootEl);
